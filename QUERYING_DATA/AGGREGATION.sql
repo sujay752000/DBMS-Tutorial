651790573ee34c17479530bad9cc7bfa102e2bb6
sujay752000 <sujayprasad010@gmail.com>
2024-06-20 23:50:04 +0530

    AGGREGATION FUNCTION is implemented

--- QUERYING_DATA/AGGREGATION.sql
+++ QUERYING_DATA/AGGREGATION.sql
@@ -2,13 +2,27 @@
 /*
 	To return a aggregated value by apply functions,
     Only returns a single value
+    
+    Other functions are : 
+	SUM(), MAX(), MIN(), AVG()
 */
+
 SELECT
 	COUNT(stu_roll) as TOTAL_STUDENTS
 FROM
 	student;
     
-/*
-Other functions are : 
-	SUM(), MAX(), MIN(), AVG()
-*/
+SELECT
+	MAX(marks_10th) as HIGHEST_MARK_IN_10th
+FROM
+	eligibility_marks;
+    
+SELECT
+	MIN(marks_12th) as LOWEST_MARK_IN_12th
+FROM
+	eligibility_marks;
+    
+SELECT
+	AVG(marks_12th) as AVERAGE_MARK_IN_12th
+FROM
+	eligibility_marks;
