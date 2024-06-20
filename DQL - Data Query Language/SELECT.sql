8c80b2cc319ed83d4c11b2dc2f2b3aae22afc071
sujay752000 <sujayprasad010@gmail.com>
2024-06-20 11:47:44 +0530

    SELECT command is implemented

---
+++ DQL - Data Query Language/SELECT.sql
@@ -0,0 +1,11 @@
+
+-- Selecting a particular columns
+SELECT course_name as COURSES, course_duration as COURSES_DURATION FROM course;
+
+-- Selecting all columns
+SELECT * FROM course;
+
+-- Performing simple calculation or operations
+SELECT now() as mytime;
+SELECT 20 + 2 as mysum;
+SELECT concat('Sujay', ' ', 'Prasad') as myname;
